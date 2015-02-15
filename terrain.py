16ff5583008f7aeaa89a67a54552bbbceffaf9b6
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2015-02-15 09:53:38 -0700

    Adding an each_feature hook

--- terrain.py
+++ terrain.py
@@ -2,7 +2,7 @@ from lettuce import *
 import ConfigParser
 import os
 
-@before.each_feature
+@before.each_feature#{{{
 def setup_config(feature):
     calling_file = feature.described_at.file # get the path to the feature that called this step
     if '/ocean/' in calling_file:
@@ -36,14 +36,19 @@ def setup_config(feature):
     world.trusted_url = world.configParser.get("trusted_repo", "test_cases_url")
 
     world.base_dir = os.getcwd()
-
     if ( world.core == "ocean" ):
         world.executable = "ocean_forward_model"
     elif ( world.core == "landice" ):
         world.executable = "landice_model"
+#}}}
 
+@before.each_step#{{{
+def setup_step(step):
+    # Change directory to base_dir
+    os.chdir(world.base_dir)
+#}}}
 
-@after.each_scenario
+@after.each_scenario#{{{
 def teardown_some_scenario(scenario):
       # print any messages that got loaded
       try:
@@ -55,7 +60,8 @@ def teardown_some_scenario(scenario):
       # reset to world.basedir so the next scenario can work
       # in case we erred and were left stranded elsewhere
       try:
-        os.chdir(world.basedir)
+        os.chdir(world.base_dir)
       except:
         pass  # In some cases, if lettuce didn't get very far, world.basedir is not defined yet.
+#}}}
 
